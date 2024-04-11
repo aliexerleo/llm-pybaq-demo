ab9ccbf9cffedb070045aceec864005854e1951c
aliexer <aliexer.mayor@sourcemeridian.com>
2024-04-10 19:31:09 -0500

    adding comments

--- app.py
+++ app.py
@@ -10,50 +10,61 @@ from langchain.chains.question_answering import load_qa_chain
 
 
 st.set_page_config('preguntaDOC')
-# title
+
+# App title
 st.header("Pregunta a tu PDF")
 
-# loaders PDF file
+# loader PDF file
 pdf_file = st.file_uploader("Carga tu documento", type="pdf",
 on_change=st.cache_resource.clear)
 
-# create embeddings with the PDF file
+# create embeddings with the PDF file 
 @st.cache_resource
 def create_embeddings(pdf):
+
     # read the PDF file
     pdf_reader = PdfReader(pdf)
     text = ""
+
     # extract the text from the PDF
     for page in pdf_reader.pages:
         text += page.extract_text()
+
     # split the text into chunks
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=800,
         chunk_overlap=100,
         length_function=len
         )
+
     # join text chunks
     join_chunks = text_splitter.split_text(text)
+
     # call embedding model
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
-    # create the knowledge base with the embeddings
+
+    # create the vectorial data base with the embeddings
     vectorial_data_base = FAISS.from_texts(join_chunks, embeddings)
 
     return vectorial_data_base
 
 if pdf_file:
-    # create the knowledge base
-    knowledge_base = create_embeddings(pdf_file)
-    # create the question answering chain
+    # create the vectorial_data_base with the PDF file
+    knowledge_data_base = create_embeddings(pdf_file)
+
+    # show text to user in UI
     user_question = st.text_input("Haz una pregunta sobre tu PDF:")
 
     if user_question:
         # search the context for the question
-        context = knowledge_base.similarity_search(user_question, 3)
-        # call the LLAMA2 model
+        context = knowledge_data_base.similarity_search(user_question, 3)
+
+        # call LLAMA2 model
         llm = Ollama(model="llama2", base_url='http://localhost:11434')
+
         # create the chain with the model
         chain = load_qa_chain(llm, chain_type="stuff")
+
         # run the chain with the context and the question
         answer = chain.run(input_documents=context, question=user_question)
 
